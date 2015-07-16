83462c17bab664234c97f4f4c8d8bc84ee8dfaf2
Gabrielle <gat2118@columbia.edu>
2015-07-16 18:30:41 -0400

    Improvements in parsing. Still issuees with nested lists.

--- improvedParseEqun.py
+++ improvedParseEqun.py
@@ -1,6 +1,6 @@
 from pyparsing import Word, Group, Optional, OneOrMore, ZeroOrMore, Suppress
 from string import ascii_uppercase, ascii_lowercase, digits
-# from pprint import pprint
+from pprint import pprint
 
 # equn = sys.argv[1]
 def convertIntegers(tokens):
@@ -14,12 +14,14 @@ def convertIntegers(tokens):
 
 element = Word(ascii_uppercase, ascii_lowercase)
 integer = Word(digits).setParseAction(convertIntegers)
-elementRef = Group(element('sym') + Optional(integer, default=1)('qty'))
-chemicalFormula = Group(OneOrMore(elementRef))
+elementRef = Group(element + Optional(integer, default=1))
+# chemicalFormula = Group(OneOrMore(elementRef))
 
 cForm = Word(ascii_uppercase, ascii_uppercase+ascii_lowercase+digits)
 equnExpr = Group(ZeroOrMore(cForm + Suppress('+')) + cForm)
-chemicalEqun = equnExpr.setResultsName('lhs') + "->" + equnExpr.setResultsName('rhs')
+lhs = equnExpr.setResultsName('lhs')
+rhs = equnExpr.setResultsName('rhs')
+chemicalEqun = lhs + "->" + rhs
 testEqun = chemicalEqun.parseString("H2 + O2 -> H2O")
 
 print(testEqun)
@@ -35,18 +37,31 @@ print
 lhsDict = {}
 temp = ()
 tempList = []
+rhsDict = {}
 
 for f in LHS:
     print(f)
-    data = elementRef.parseString(f)
+    data = elementRef.parseString(f).asList()
+    print(data)
     for d in data:
-        temp = (data.sym, data.qty)
+        temp = tuple(data)
         print(temp)
         tempList.append(temp)
     lhsDict[f] = tempList
     temp = ()
     tempList = []
     
+for f in RHS:
+    print(f)
+    data = elementRef.parseString(f).asList()
+    for d in data:
+        temp = tuple(data)
+        print(temp)
+        tempList.append(temp)
+    rhsDict[f] = tempList
+    temp = ()
+    tempList = []
 
 print(lhsDict)
+print(rhsDict)
 print
