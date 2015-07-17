13d14fff573d4d82af32d0ba9939ae2c615e4105
Gabrielle Taylor <gat2118@columbia.edu>
2015-07-17 13:58:05 -0400

    Style fixes. Added new file - buildMatrix

--- improvedParseEqun.py
+++ improvedParseEqun.py
@@ -10,22 +10,22 @@ def parseEqun(equation):
     integer = Word(digits).setParseAction(convertIntegers)
     elementRef = Group(element + Optional(integer, default=1))
     chemicalFormula = OneOrMore(elementRef)
-    
+
     cForm = Word(ascii_uppercase, ascii_uppercase + ascii_lowercase + digits)
     equnExpr = Group(ZeroOrMore(cForm + Suppress('+')) + cForm)
     lhs = equnExpr.setResultsName('lhs')
     rhs = equnExpr.setResultsName('rhs')
     chemicalEqun = lhs + "->" + rhs
     parsedEqun = chemicalEqun.parseString(equation)
-    
+
     LHS = parsedEqun['lhs'].asList()
     RHS = parsedEqun['rhs'].asList()
-    
+
     lhsDict = {}
     rhsDict = {}
     tempDict = defaultdict(int)
     tempList = []
-    
+
     for f in LHS:
         tempList = chemicalFormula.parseString(f).asList()
         print(tempList)
