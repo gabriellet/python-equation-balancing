77ea0f1086e1f9097fe87d507390cfe3913cfac6
Gabrielle Taylor <gat2118@columbia.edu>
2015-07-17 16:51:12 -0400

    style fixes

--- buildMatrix.py
+++ buildMatrix.py
@@ -1,18 +1,25 @@
+from itertools import chain
+import sympy as sp
 
+def buildMatrix(lhs, rhs):
 
-# figure out number columns
-cols = len(lhs) + len(rhs)
+    # figure out number columns
+    cols = len(lhs) + len(rhs)
 
+    # figure out number rows
+    l = []
+    for c in lhs:
+        l = chain(l, list(lhs[c].keys()))
 
-# figure out number rows
-l = []
-for c in lhs:
-    l = chain(l, list(lhs[c].keys()))
+    for c in rhs:
+        l = chain(l, list(rhs[c].keys()))
 
-for c in rhs:
-    l = chain(l, list(lhs[c].keys()))
+    t = list(l)
+    r = set(t)
+    rows = len(r)
 
-t = set(list(l))
-rows = t.len()
+    mat = sp.zeros(rows, cols)
 
-
+    print(rows)
+    print(cols)
+    print(mat)
