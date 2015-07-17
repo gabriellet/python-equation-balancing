6e95bf9ea65bda5e385b6cbb6f9d92da7bd0cdba
Gabrielle Taylor <gat2118@columbia.edu>
2015-07-17 12:33:51 -0400

    Parsing of equation implemented, results correctly structured

--- improvedParseEqun.py
+++ improvedParseEqun.py
@@ -1,53 +1,47 @@
 from pyparsing import Word, Group, Optional, OneOrMore, ZeroOrMore, Suppress
 from string import ascii_uppercase, ascii_lowercase, digits
-# from pprint import pprint
+from collections import defaultdict
 
-# equn = sys.argv[1]
 def convertIntegers(tokens):
     return int(tokens[0])
 
-# ele = Word(uppercase, lowercase)
-# inte = Word(digits).setParseAction(convertIntegers)
-# element = Group(ele + Optional(inte, default=1))
-# chemGroup = Group(Suppress('(') + OneOrMore(element) + Suppress(')')
-# term =
+def parseEqun(equation):
+    element = Word(ascii_uppercase, ascii_lowercase)
+    integer = Word(digits).setParseAction(convertIntegers)
+    elementRef = Group(element + Optional(integer, default=1))
+    chemicalFormula = OneOrMore(elementRef)
+    
+    cForm = Word(ascii_uppercase, ascii_uppercase + ascii_lowercase + digits)
+    equnExpr = Group(ZeroOrMore(cForm + Suppress('+')) + cForm)
+    lhs = equnExpr.setResultsName('lhs')
+    rhs = equnExpr.setResultsName('rhs')
+    chemicalEqun = lhs + "->" + rhs
+    parsedEqun = chemicalEqun.parseString(equation)
+    
+    LHS = parsedEqun['lhs'].asList()
+    RHS = parsedEqun['rhs'].asList()
+    
+    lhsDict = {}
+    rhsDict = {}
+    tempDict = defaultdict(int)
+    tempList = []
+    
+    for f in LHS:
+        tempList = chemicalFormula.parseString(f).asList()
+        print(tempList)
+        for k, v in tempList:
+            tempDict[k] += v
+            print(tempDict)
+        lhsDict[f] = tempDict
+        tempDict = defaultdict(int)
 
-element = Word(ascii_uppercase, ascii_lowercase)
-integer = Word(digits).setParseAction(convertIntegers)
-elementRef = Group(element + Optional(integer, default=1))
-chemicalFormula = OneOrMore(elementRef)
+    for f in RHS:
+        tempList = chemicalFormula.parseString(f).asList()
+        print(tempList)
+        for k, v in tempList:
+            tempDict[k] += v
+            print(tempDict)
+        rhsDict[f] = tempDict
+        tempDict = defaultdict(int)
 
-cForm = Word(ascii_uppercase, ascii_uppercase + ascii_lowercase + digits)
-equnExpr = Group(ZeroOrMore(cForm + Suppress('+')) + cForm)
-lhs = equnExpr.setResultsName('lhs')
-rhs = equnExpr.setResultsName('rhs')
-chemicalEqun = lhs + "->" + rhs
-testEqun = chemicalEqun.parseString("H2 + O2 -> H2O")
-
-print(testEqun)
-print("LHS: ")
-LHS = testEqun['lhs'].asList()
-print(LHS)
-print("RHS: ")
-RHS = testEqun['rhs'].asList()
-print(RHS)
-
-print
-
-lhsDict = {}
-temp = ()
-dataDict = {}
-tempList = []
-rhsDict = {}
-
-for f in LHS:
-    print(f)
-    lhsDict[f] = dict(chemicalFormula.parseString(f).asList())
-
-for f in RHS:
-    print(f)
-    rhsDict[f] = dict(chemicalFormula.parseString(f).asList())
-
-print(lhsDict)
-print(rhsDict)
-print
+    return lhsDict, rhsDict
